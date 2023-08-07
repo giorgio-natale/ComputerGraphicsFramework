daba23e24f992aa068b8a710c4e28c356060cead
Matilde Brugnano <matilde.brugnano@smg-team.net>
2023-08-07 18:44:44 +0200

    Add BulletSpawnerTest

---
+++ demo/BulletSpawnerTest.h
@@ -0,0 +1,27 @@
+//
+// Created by Matilde on 07/08/2023.
+//
+
+#ifndef DEMO_BulletSpawnerTest_H
+#define DEMO_BulletSpawnerTest_H
+
+#include "../Starter.hpp"
+
+class BulletSpawnerTest : public BaseProject {
+protected:
+
+    // Current aspect ratio (used by the callback that resized the window
+    float Ar;
+
+    void setWindowParameters() override;
+    void onWindowResize(int w, int h) override;
+    void localInit() override;
+    void pipelinesAndDescriptorSetsInit() override;
+    void pipelinesAndDescriptorSetsCleanup() override;
+    void localCleanup() override;
+    void populateCommandBuffer(VkCommandBuffer commandBuffer, int currentImage) override;
+    void updateUniformBuffer(uint32_t currentImage) override;
+
+};
+
+#endif //DEMO_BulletSpawnerTest_H
