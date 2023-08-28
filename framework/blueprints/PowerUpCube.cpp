02d5ac3f0563094f792ef516fb6bb86031863225
drgio <giorgiobuttiglieri99@gmail.com>
2023-08-28 15:09:09 +0200

    Increase boss spawner collider radius

--- framework/blueprints/PowerUpCube.cpp
+++ framework/blueprints/PowerUpCube.cpp
@@ -25,26 +25,30 @@ namespace fmwk {
         powerUpEntity->addComponent(std::make_unique<fmwk::MeshComponent>(gameEngine->getModelByName("myCube")));
         powerUpEntity->getTransform().setScale(glm::vec3(0.8f, 0.8f, 0.8f));
         powerUpEntity->addComponent(std::make_unique<fmwk::SimplePhongMaterial>());
-        powerUpEntity->addComponent(std::make_unique<fmwk::Collider>(1.0f, "POWER_UP", glm::vec3(0, 0, 0)));
         powerUpEntity->addComponent(std::make_unique<fmwk::TriggerPowerUp>());
         switch (_type) {
             case SET_SHIELD:
+                powerUpEntity->addComponent(std::make_unique<fmwk::Collider>(1.0f, "POWER_UP", glm::vec3(0, 0, 0)));
                 powerUpEntity->addComponent(std::make_unique<fmwk::TextureComponent>(gameEngine->getBoundTextureByName("powerUpShield")));
                 powerUpEntity->addComponent(std::make_unique<fmwk::PowerUpSetShield>());
                 break;
             case INCREASE_SPEED:
+                powerUpEntity->addComponent(std::make_unique<fmwk::Collider>(1.0f, "POWER_UP", glm::vec3(0, 0, 0)));
                 powerUpEntity->addComponent(std::make_unique<fmwk::TextureComponent>(gameEngine->getBoundTextureByName("powerUpSpeedUp")));
                 powerUpEntity->addComponent(std::make_unique<fmwk::PowerUpIncreaseSpeed>());
                 break;
             case DECREASE_BULLET_COOL_DOWN:
+                powerUpEntity->addComponent(std::make_unique<fmwk::Collider>(1.0f, "POWER_UP", glm::vec3(0, 0, 0)));
                 powerUpEntity->addComponent(std::make_unique<fmwk::TextureComponent>(gameEngine->getBoundTextureByName("powerUpBullet")));
                 powerUpEntity->addComponent(std::make_unique<fmwk::PowerUpDecreaseBulletCoolDown>());
                 break;
             case ADD_LIFE:
+                powerUpEntity->addComponent(std::make_unique<fmwk::Collider>(1.0f, "POWER_UP", glm::vec3(0, 0, 0)));
                 powerUpEntity->addComponent(std::make_unique<fmwk::TextureComponent>(gameEngine->getBoundTextureByName("powerUpLife")));
                 powerUpEntity->addComponent(std::make_unique<fmwk::PowerUpAddLife>());
                 break;
             case SPAWN_BOSS_ENEMY:
+                powerUpEntity->addComponent(std::make_unique<fmwk::Collider>(3.0f, "POWER_UP", glm::vec3(0, 0, 0)));
                 powerUpEntity->addComponent(std::make_unique<fmwk::TextureComponent>(gameEngine->getBoundTextureByName("white")));
                 powerUpEntity->addComponent(std::make_unique<fmwk::PowerUpSpawnBossEnemy>());
                 powerUpEntity->getTransform().setScale(glm::vec3(6));
